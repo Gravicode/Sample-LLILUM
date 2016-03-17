c0d0b2912afd23f0ec6898083d033da7c99aa524
mifmasterz <mifmasterz@yahoo.com>
2016-03-17 09:39:16 +0700

    latest compiled
    ok

---
+++ SampleLlilum/SampleLlilum/Managed/Program.cs
@@ -0,0 +1,49 @@
+﻿//
+// Copyright (c) Microsoft Corporation.    All rights reserved.
+//
+
+//#define LPC1768
+//#define K64F
+//#define STM32F411
+#define STM32F401
+//#define STM32F091
+
+namespace Managed
+{
+    using System;
+    using Windows.Devices.Gpio;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+
+#if LPC1768
+    using Microsoft.Llilum.LPC1768;
+#elif K64F
+    using Microsoft.Llilum.K64F;
+#elif STM32F411
+    using Microsoft.Llilum.STM32F411;
+#elif STM32F401
+    using Microsoft.Llilum.STM32F401;
+#elif STM32F091
+    using Microsoft.Llilum.STM32F091;
+#endif
+    
+    class Program
+    {
+        static void Main()
+        {
+            var ctlr = GpioController.GetDefault();
+
+            var gpioPin = ctlr.OpenPin((int)PinName.D2);
+            
+            gpioPin.SetDriveMode(GpioPinDriveMode.Output);
+            
+            while (true)
+            {
+                gpioPin.Write(GpioPinValue.High);
+                Thread.Sleep(200);
+                gpioPin.Write(GpioPinValue.Low);
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
---
+++ SampleLlilum/SampleLlilum/Native/tmp_debug.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\GNU Tools ARM Embedded\5.2 2015q4\bin\arm-none-eabi-gdb.exe" "E:\Experiment\IOT4BDG\SampleLlilum\ARM\Debug\SampleLlilum.elf"  %*
